9b2287826428524aa0dc5e699ed44d11fb82cd43
Roma Sabirov <erikbeyl.00@gmail.com>
2024-08-27 22:04:16 +0500

    работа "срдний балл"

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from module_1_6 import module_1_6
 
-module_1_6()
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(list(students))
+avg_grades = [0,0,0,0,0]
+journal = {}
+a = 0
+l = 0
+for i in range(len(grades)):
+    a = 0
+    for j in range(len(grades[i])):
+        a += grades[i][j]
+        l = len(grades[i])
+    avg_grades[i] = a / l
+    journal[students[i]] = avg_grades[i]
+print(journal)
+
