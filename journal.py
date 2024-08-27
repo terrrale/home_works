a9ac7a7dd15d432586a429b4982d6ea1bf99cb69
Roma Sabirov <erikbeyl.00@gmail.com>
2024-08-27 21:59:38 +0500

    работа "срдний балл"

--- journal.py
+++ journal.py
@@ -6,5 +6,5 @@ avg_grades_1 = [sum(grades[0])/len(grades[0]),
     sum(grades[2]) /len(grades[2]),
     sum(grades[3]) /len(grades[3]),
     sum(grades[4]) /len(grades[4])]
-journal = {students[0]: avg_grades_1[0], students[1]: avg_grades_1[1], students[2]: avg_grades_1[2], students[3]: avg_grades_1[3], students[4]: avg_grades_1[4]}
-print(journal)
+journal = zip(students, avg_grades_1)
+print(dict(journal))
