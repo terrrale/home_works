f1394446968fd7d7399a7deae73d1486c1cba213
Roma Sabirov <erikbeyl.00@gmail.com>
2024-08-31 20:36:30 +0500

    Работа "все ли равны?" с исправлением

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,13 @@
-first, second, third = input('Введите три числа: ').split()
+
+#first, second, third = input('Введите три числа: ').split()
+first = input("Введите первое число: ")
+second = input("Введите второе число: ")
+third = input("Введите третье число: ")
 print( first, second, third)
-if first != second and third:
-    print(0)
+if first == second and third:
+    print(3)
 else:
-    if  first == second or third:
-        print(3)
+    if first == second and third:
+        print(0)
     else:
         print(2)
