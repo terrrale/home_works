6b6a85a1c5fe26642123302f8a822a099f3c69d4
Roma Sabirov <erikbeyl.00@gmail.com>
2024-09-05 01:42:09 +0500

    пространства имён

--- module_3_1.py
+++ module_3_1.py
@@ -14,12 +14,13 @@ def is_contains(string, list_to_search):
     count_calls()
     list(list_to_search)
     print(string)
-    for i in list_to_search:
-        print(i)
-        if string.lower() in i.lower():
-            return True
-        else:
-            return False
+    new_list = [s.lower() for s in list_to_search ]
+    # for i in list_to_search:
+    #     print(i)
+    if string.lower() in new_list:
+        return True
+    else:
+        return False
 
 print(string_info('weapon'))
 print(is_contains('pAnda', ['PanDa', 'tiger', 'Dog']))
