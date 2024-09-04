75a1b966f550f59a63414aa098ab9ab579ac373b
terrrale <100957573+terrrale@users.noreply.github.com>
2024-09-05 01:47:41 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -15,8 +15,6 @@ def is_contains(string, list_to_search):
     list(list_to_search)
     print(string)
     new_list = [s.lower() for s in list_to_search ]
-    # for i in list_to_search:
-    #     print(i)
     if string.lower() in new_list:
         return True
     else:
