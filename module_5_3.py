16294edf4e77216092c053049758b2b4900c4458
Roma Sabirov <erikbeyl.00@gmail.com>
2025-01-12 21:42:15 +0500

    готово

--- module_5_3.py
+++ module_5_3.py
@@ -77,20 +77,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2)
+print(h1 == h2) # __eq__
 
-h1 = h1 + 10
+h1 = h1 + 10 # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10
+h1 += 10 # __iadd__
 print(h1)
 
-h2 = 10 + h2
+h2 = 10 + h2 # __radd__
 print(h2)
 
-print(h1 > h2)
-print(h1 >= h2)
-print(h1 < h2)
-print(h1 <= h2)
-print(h1 != h2)
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
